8cbbbc74c8dbd865f237fa6e955c2392520acdcc
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-06-04 20:41:12 +0200

    urgent deps fix

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 setup(name='michi-mpris',
-      version='1.0.4',
+      version='1.0.5',
       author='Michał Sidor',
       author_email='michcioperz@gmail.com',
       description='wrapper over MPRIS2 APIs',
       url='http://github.com/michcioperz/mpris',
-      py_modules=['michimpris'],
+      py_modules=['michimpris','michitomahawk','michispotify'],
       scripts=['michispotify.py','michitomahawk.py'],
       )
